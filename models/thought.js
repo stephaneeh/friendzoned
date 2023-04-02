5261da92f91f3bfa1e0d906e02143e1b83b84db4
Stephanee Hayden <stephaneehayden@gmail.com>
2023-04-02 11:08:44 +1000

    models updated

---
+++ models/thought.js
@@ -0,0 +1,73 @@
+const { Schema, Types, model } = require("mongoose");
+
+function formatDate(date) {
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return date.toLocaleDateString("en-US", options);
+}
+
+const reactionSchema = new Schema({
+  reactionID: {
+    type: Types.ObjectId,
+    default: new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    Required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    get: formatDate,
+  },
+});
+
+// Schema to create Though model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: formatDate,
+    },
+    username: {
+      type: String,
+      Required: true,
+    },
+    reactions: [reactionSchema],
+  }
+  // { FIXME: IS THIS REQUIRED?
+  //   toJSON: {
+  //     virtuals: true,
+  //     getters: true,
+  //   },
+  //   id: false,
+  // }
+);
+
+// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+thoughtSchema
+  .virtual("reactionCount")
+  // Getter
+  .get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize our Thought model
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
--- models/user.js
+++ models/user.js
@@ -42,7 +42,6 @@ const userSchema = new Schema(
     toJSON: {
       virtuals: true,
     },
-    id: false,
   }
 );
 

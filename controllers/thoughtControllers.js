46b23d40f0e70c728e42ca1f67df124296ee4e9b
Stephanee Hayden <stephaneehayden@gmail.com>
2023-04-09 12:35:54 +1000

    correction to thought controllers

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -1,5 +1,4 @@
-const { ObjectId } = require("mongoose").Types;
-const { User, Thought } = require("../models");
+const { User, Thought, reactionSchema } = require("../models");
 
 // Aggregate function to get the number of students overall
 const headCount = async () =>
@@ -11,6 +10,7 @@ module.exports = {
   // TODO: GET to get all thoughts
   getThoughts(req, res) {
     Thought.find()
+      .populate("username")
       .then((thoughts) => res.json(thoughts))
       .catch((err) => {
         console.log(err);
@@ -21,7 +21,7 @@ module.exports = {
   // TODO: GET to get a single thought by its _id
   getSingleThought(req, res) {
     Thought.findOne({ _id: req.params.thoughtId })
-      .select("-__v")
+      .populate("username")
       .then((thought) =>
         !thought
           ? res.status(404).json({ message: "No thought with that ID" })
@@ -33,18 +33,21 @@ module.exports = {
   // TODO: POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
   createThoughts(req, res) {
     console.log("You are adding a thought");
-    console.log(req.body);
-    User.findOneAndUpdate(
-      { _id: req.body.userId },
-      { $push: { thoughts: _id } },
-      { new: true }
-    )
-      .then((thought) =>
-        !thought
-          ? res.status(404).json({ message: "No thought found with this ID!" })
-          : res.json(thought)
-      )
-      .catch((err) => res.status(500).json(err));
+    Thought.create(req.body)
+      .then((dbThoughtData) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { thoughts: dbThoughtData._id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: "No User with this id!" });
+        }
+        res.json({ message: "You have created a thought!" });
+      })
+      .catch((err) => res.status(500).json({ message: err.message }));
   },
 
   // PUT to update a thought by its _id
@@ -54,6 +57,7 @@ module.exports = {
       { $set: req.body },
       { runValidators: true, New: true }
     )
+      .populate("reactions")
       .then((thought) =>
         !thought
           ? res.status(404).json({ message: "No thought found with this ID!" })
@@ -66,19 +70,13 @@ module.exports = {
     Thought.findOneAndDelete({ _id: req.params.thoughtId })
       .then((thought) =>
         !thought
-          ? res.status(404).json({ message: "No thought found with this ID!" })
-          : User.findOneAndUpdate(
-              { thoughts: req.params.thoughtId },
-              { $pull: { thoughts: req.params.thoughtId } },
-              { new: true }
-            )
+          ? res.status(404).json({ message: "No thought with this id!" })
+          : reactionSchema.deleteMany({ _id: { $in: thought.reactions } })
       )
-      .then((user) =>
-        !user
-          ? res
-              .status(404)
-              .json({ message: "Thought deleted, but no user found" })
-          : res.json({ message: "Thought successfully deleted" })
+      .then(() =>
+        res.json({
+          message: "Thought with this id and its reactions are deleted",
+        })
       )
       .catch((err) => res.status(500).json(err));
   },
